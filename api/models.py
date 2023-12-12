a6601e759e267af696c1d077efee710ae7b7c221
Azan10 <98818090+Azan10@users.noreply.github.com>
2023-12-12 15:19:53 +0000

    Delete api directory

--- api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class CustomUser(AbstractUser):
-
-    profile_image = models.ImageField(upload_to='profile_images/', default='default.jpg', blank=True)
-    email = models.EmailField(blank=True, null=True)  
-
-    date_of_birth = models.DateField(null=True, blank=True)
